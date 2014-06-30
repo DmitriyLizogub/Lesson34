13dfa1e37f3b14ae7343583481d5329c400f895d
Dmitriy Lizogub <dlizogub@accesssoftek.com>
2014-06-30 17:01:05 +0300

    BinarySearche in array leftUnion

--- src/main/java/com/dmitriy/lesson34/ArrayHelper.java
+++ src/main/java/com/dmitriy/lesson34/ArrayHelper.java
@@ -8,27 +8,88 @@ import java.util.Arrays;
 public class ArrayHelper implements LeftUnion {
 
     @Override
-    public int[] leftUnion(int[] a, int[] b) {
+    public int[] leftUnion(int[] leftArray, int[] rightArray) {
 
-        int sumLength = a.length + b.length;
-        int[] c = Arrays.copyOf(a, sumLength);
-        int count = a.length;
+        int resultLength = leftArray.length + rightArray.length;
+        int [] resultArray = Arrays.copyOf(leftArray, resultLength);
+        int count = leftArray.length;
+        Arrays.sort(resultArray);
 
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b.length; j++)
-
-                if (c[i] == b[j]) {
-                    c[count] = b[j];
-                    count++;
-                }
-        }
-
-        int[] d = new int[0];
-        for (int k = c.length; k > 0; k--) {
-            if (c[k - 1] == 0) {
-                d = Arrays.copyOf(c, k - 1);
+        for (int i = 0; i < rightArray.length; i++) {
+            if(Arrays.binarySearch(resultArray, rightArray[i]) >= 0){
+                resultArray[i] = rightArray[i];
+                count++;
             }
         }
-        return d;
+        return removeUnneededZerosFromArray(leftArray, rightArray, resultArray);
     }
+
+
+
+
+
+    private int[] removeUnneededZerosFromArray(int[] leftArray, int[] rightArray, int[] resultArray) {
+        int zerosInResultArray = countAllZerosInArray(resultArray);
+        int neededZeros = countNeededZeros(leftArray, rightArray);
+        int numberOfElementsForRemove = zerosInResultArray - neededZeros;
+        int newArrayLength = resultArray.length - numberOfElementsForRemove;
+
+        for (int i = 0; i < resultArray.length; i++) {
+            if (resultArray[i] == 0) {
+                resultArray[i] = -2147483648;
+            }
+        }
+
+        Arrays.sort(resultArray);
+
+        int[] destArray = new int[newArrayLength];
+        System.arraycopy(resultArray, numberOfElementsForRemove, destArray, 0, newArrayLength);
+
+        for (int t = 0; t < neededZeros; t++) {
+            destArray[t] = 0;
+        }
+        return destArray;
+    }
+
+    private int countNeededZeros(int[] leftArray, int[] rightArray) {
+        int counter = 0;
+
+        if (isZerosInArray(leftArray)) {
+            for (int k = 0; k < leftArray.length; k++) {
+                if (leftArray[k] == 0) counter++;
+            }
+            if (isZerosInArray(rightArray)) {
+                for (int z = 0; z < rightArray.length; z++) {
+                    if (rightArray[z] == 0) counter++;
+                }
+            }
+        }
+        return counter;
+
+    }
+
+    private int countAllZerosInArray(int[] inputArray) {
+        int coun = 0;
+
+        if (isZerosInArray(inputArray)) {
+            for (int k = 0; k < inputArray.length; k++) {
+                if (inputArray[k] == 0) coun++;
+            }
+
+        }
+        return coun;
+
+    }
+
+    private boolean isZerosInArray(int[] inputArray) {
+        boolean isZeros = false;
+        for (int k = 0; k < inputArray.length; k++) {
+            if (inputArray[k] == 0) {
+                isZeros = true;
+                break;
+            }
+        }
+        return isZeros;
+    }
+
 }
--- src/main/java/com/dmitriy/lesson34/MainArray.java
+++ src/main/java/com/dmitriy/lesson34/MainArray.java
@@ -8,8 +8,8 @@ import java.util.Arrays;
 public class MainArray {
 
     public static void main(String [] args){
-        int [] leftArray = {1,5,4,23,65,32,78};
-        int [] rightArray = {3,5,24,4,1,2,34,45,32,5};
+        int [] leftArray = {1,5,4,5,23,65,32,78,0,0,-49,5,555,3478};
+        int [] rightArray = {7,3,5,0,0,23,5,4,1,34,45,32,-49};
 
         ArrayHelper arrayHelper = new ArrayHelper();
         int [] resultArray = arrayHelper.leftUnion(leftArray, rightArray);
